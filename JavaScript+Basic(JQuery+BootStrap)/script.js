1580b113cd9eb263a2c89e5d6ffc36b13f2781ea
LoNERIdeR <85149949+Lone09Rider@users.noreply.github.com>
2022-07-14 17:16:58 +0530

    Add files via upload

---
+++ JavaScript+Basic(JQuery+BootStrap)/script.js
@@ -0,0 +1,11 @@
+alert("Page Loaded, JS AlerT !!");
+
+function buttonClicked() {
+    alert("I am Clicked !!");
+    document.getElementById("title").innerHTML = "AFTER CLICKING ME";
+    document.getElementsByClassName("btn").innerText = "CLICKED !!"
+}
+
+function btnclick() {
+    alert("Submitted Successfully. Redirecting .... !!");
+}
